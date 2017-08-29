f6936837a8af9cf1a110d7ce1bb821c4c1fddbe7
rodolfovilaca <rodolfovilaca91@gmail.com>
2017-08-29 00:13:24 -0300

    minor changes

--- Java CodingBat Functional-2/src/Exercises.java
+++ Java CodingBat Functional-2/src/Exercises.java
@@ -3,54 +3,73 @@ import java.util.stream.Collectors;
 
 public class Exercises {
 	public static List<Integer> noNeg(List<Integer> nums) {
-		return nums.stream().filter((num) -> num >= 0).collect(Collectors.toList());
+		return nums.stream()
+				.filter((num) -> num >= 0)
+				.collect(Collectors.toList());
 	}
 
 	public static List<Integer> no9(List<Integer> nums) {
-		return nums.stream().filter(n -> n % 10 != 9).collect(Collectors.toList());
+		return nums.stream()
+				.filter(n -> n % 10 != 9)
+				.collect(Collectors.toList());
 	}
 
 	public static List<Integer> noTeen(List<Integer> nums) {
-		return nums.stream().filter(n -> n < 13 || n > 19).collect(Collectors.toList());
+		return nums.stream()
+				.filter(n -> n < 13 || n > 19)
+				.collect(Collectors.toList());
 	}
 
 	public static List<String> noLong(List<String> strings) {
-		return strings.stream().filter(string -> string.length() < 4).collect(Collectors.toList());
+		return strings.stream()
+				.filter(string -> string.length() < 4)
+				.collect(Collectors.toList());
 	}
 
 	public static List<String> noZ(List<String> strings) {
-		return strings.stream().filter(string -> {
-			for (int i = 0; i < string.length(); i++) {
-				if (string.charAt(i) == 'z') {
-					return false;
-				}
-			}
-			return true;
-		}).collect(Collectors.toList());
+		return strings.stream()
+				.filter(string -> {
+						for (int i = 0; i < string.length(); i++) {
+							if (string.charAt(i) == 'z') {
+								return false;
+							}
+						}
+						return true;
+					})
+				.collect(Collectors.toList());
 	}
 
 	public static List<String> no34(List<String> strings) {
-		return strings.stream().filter(string -> string.length() < 3 || string.length() > 4)
+		return strings.stream()
+				.filter(string -> string.length() < 3 || string.length() > 4)
 				.collect(Collectors.toList());
 	}
 
 	public static List<String> noYY(List<String> strings) {
-		return strings.stream().map(string -> string + "y").filter(string -> {
-			for (int i = 0; i < string.length() - 1; i++) {
-				if (string.charAt(i) == 'y' && string.charAt(i + 1) == 'y') {
-					return false;
-				}
-			}
-			return true;
-		}).collect(Collectors.toList());
+		return strings.stream()
+				.map(string -> string + "y")
+				.filter(string -> {
+						for (int i = 0; i < string.length() - 1; i++) {
+							if (string.charAt(i) == 'y' && string.charAt(i + 1) == 'y') {
+								return false;
+							}
+						}
+						return true;
+					})
+				.collect(Collectors.toList());
 	}
 
 	public static List<Integer> two2(List<Integer> nums) {
-		return nums.stream().map(num -> num * 2).filter(num -> num % 10 != 2).collect(Collectors.toList());
+		return nums.stream()
+				.map(num -> num * 2)
+				.filter(num -> num % 10 != 2)
+				.collect(Collectors.toList());
 	}
 
 	public static List<Integer> square56(List<Integer> nums) {
-		return nums.stream().map(num -> (num * num) + 10).filter(num -> num % 10 != 5 && num % 10 != 6)
+		return nums.stream()
+				.map(num -> (num * num) + 10)
+				.filter(num -> num % 10 != 5 && num % 10 != 6)
 				.collect(Collectors.toList());
 	}
 }
